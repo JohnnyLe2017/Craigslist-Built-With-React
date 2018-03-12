71743071531bf329a749c7d6f6ed854f54ff0876
JohnnyLe2017 <lejohnn2@gmail.com>
2018-03-12 19:51:47 -0400

    Commit

--- assets/js/craigslist/App.js
+++ assets/js/craigslist/App.js
@@ -9,7 +9,7 @@ import Header from './includes/Header.js'
 import Home from './pages/Home.js'
 import Listings from './pages/Listings.js'
 import Category from './pages/Category.js'
-import Item from './pages/Item.js'
+import Details from './pages/Details.js'
 
 
 
@@ -33,7 +33,7 @@ export default class App extends Component {
         <Route exact path="/:city" component={Home} />
         <Route exact path="/:city/:category" component={Category} />
         <Route exact path="/:city/:category/:listings" component={Listings} />
-        <Route exact path="/:city/:category/:listings/:item" component={Item} />
+        <Route exact path="/:city/:category/:listings/:item" component={Details} />
         </div>
       </Router>
   )
---
+++ assets/js/craigslist/pages/Details.js
@@ -0,0 +1,48 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+
+export default class Details extends Component {
+  constructor () {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render () {
+    const {match, location, history} = this.props
+    return (
+      <div className="details-page">
+      <div className="container">
+      <section className="submenu">
+        <div className="direction">
+        <div className="prev">Prev</div>
+        <div className="next">Next</div>
+        </div>
+
+      <nav className="sub-links">
+        <a href="#">More Ads by User</a>
+        <a href="#">Print</a>
+        <a href="#">Share</a>
+        <a href="#">Contact Seller</a>
+        </nav>
+      </section>
+
+      <section>
+        <div className="media-column">
+          <div className="gallery">
+            <div className="main-image">
+              
+            </div>
+          </div>
+        </div>
+        <div className="details-column">
+
+        </div>
+      </section>
+      </div>
+      </div>
+    )
+    }
+}
