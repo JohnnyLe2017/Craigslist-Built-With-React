6ee4169ea8b2a6b68335469087c04121b6fa8269
JohnnyLe2017 <lejohnn2@gmail.com>
2018-03-16 14:07:04 -0400

    Commit

--- assets/js/craigslist/pages/components/Gallery.js
+++ assets/js/craigslist/pages/components/Gallery.js
@@ -5,16 +5,27 @@ export default class Gallery extends Component {
   constructor() {
     super()
     this.state = {
-      allImages: [
-        'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_5.jpg',
-        'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_6.jpg',
-        'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_7.jpg',
-        'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_12.jpg',
-        'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_13.jpg',
-        'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_14.jpg'
-      ]
+      allImages: '',
+      currentImage: '',
+      currentIndex: 0
     }
   }
+
+  componentWillMount() {
+    const allImages = [
+      'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_5.jpg',
+      'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_6.jpg',
+      'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_7.jpg',
+      'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_12.jpg',
+      'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_13.jpg',
+      'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_14.jpg'
+    ]
+
+    this.setState({
+      allImages,
+      currentImage: allImages[this.state.currentIndex]
+    })
+}
 
   allImagesLoop = () => {
     console.log(this.state.allImages)
@@ -27,16 +38,28 @@ export default class Gallery extends Component {
 	})
 	}
 
+  nextBtn = () => {
+    this.setState({
+      currentIndex: this.state.currentIndex + 1
+    })
+  }
+
+  prevBtn = () => {
+    this.setState({
+      currentIndex: this.state.currentIndex - 1
+    })
+  }
+
   render() {
     const { match, location, history } = this.props
     return (
               <div className="gallery">
                 <div className="slider">
                   <div className="main-image">
-                    <div className="arrows left-arrow">{"<"}</div>
-                    <div className="arrows right-arrow">{">"}</div>
+                    <div className="arrows left-arrow" onClick={this.prevBtn}>{"<"}</div>
+                    <div className="arrows right-arrow" onClick={this.nextBtn}>{">"}</div>
                       <div className="image-1" style={{
-                             "backgroundImage": `url('${this.state.allImages[0]}')`
+                             "backgroundImage": `url('${this.state.allImages[this.state.currentIndex]}')`
                       }}></div>
                   </div>
                 </div>
--- public/js/components/craigslist.js
+++ public/js/components/craigslist.js
@@ -1044,13 +1044,37 @@ var Gallery = function (_Component) {
       });
     };
 
+    _this.nextBtn = function () {
+      _this.setState({
+        currentIndex: _this.state.currentIndex + 1
+      });
+    };
+
+    _this.prevBtn = function () {
+      _this.setState({
+        currentIndex: _this.state.currentIndex - 1
+      });
+    };
+
     _this.state = {
-      allImages: ['https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_5.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_6.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_7.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_12.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_13.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_14.jpg']
+      allImages: '',
+      currentImage: '',
+      currentIndex: 0
     };
     return _this;
   }
 
   _createClass(Gallery, [{
+    key: "componentWillMount",
+    value: function componentWillMount() {
+      var allImages = ['https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_5.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_6.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_7.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_12.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_13.jpg', 'https://cars.usnews.com/static/images/Auto/custom/13372/2017_BMW_5-Series_14.jpg'];
+
+      this.setState({
+        allImages: allImages,
+        currentImage: allImages[this.state.currentIndex]
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
       var _props = this.props,
@@ -1069,16 +1093,16 @@ var Gallery = function (_Component) {
             { className: "main-image" },
             _react2.default.createElement(
               "div",
-              { className: "arrows left-arrow" },
+              { className: "arrows left-arrow", onClick: this.prevBtn },
               "<"
             ),
             _react2.default.createElement(
               "div",
-              { className: "arrows right-arrow" },
+              { className: "arrows right-arrow", onClick: this.nextBtn },
               ">"
             ),
             _react2.default.createElement("div", { className: "image-1", style: {
-                "backgroundImage": "url('" + this.state.allImages[0] + "')"
+                "backgroundImage": "url('" + this.state.allImages[this.state.currentIndex] + "')"
               } })
           )
         ),
