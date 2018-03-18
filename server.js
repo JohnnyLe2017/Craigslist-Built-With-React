6c86d1be2eda1e9d9a440aad113237eeeafe52b3
Johnny Le <johnny@Johnnys-MBP.attlocal.net>
2018-03-18 13:07:45 -0400

    Commit

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ const app = express()
 
 // Create middleware to handle the serving of the app
 app.use('/', serveStatic(path.join(__dirname, '/public')))
-app.get('/api/categories', function (req, res) {
+
+// api
+app.get('/api/cities', function (req, res) {
   res.json(categoriesData)
 })
+
+app.get('/api/:city/:categories', function (req, res) {
+  res.json(categoriesData)
+})
+
+app.get('/api/:city/:categories/:listing/items', function (req, res) {
+  res.json(categoriesData)
+})
+
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
