3d125971dd3bebae3d334b9569f7e362b5e8b379
Johnny Le <johnny@MacBook-Pro.attlocal.net>
2018-03-17 21:44:18 -0400

    Commit

--- data/categories.js
+++ data/categories.js
@@ -33,30 +33,6 @@ module.exports = [
       {
         name: 'local news',
         slug: 'local-news'
-      },
-      {
-        name: 'lost & found',
-        slug: 'lost-and-found'
-      },
-      {
-        name: 'musicians',
-        slug: 'musicians'
-      },
-      {
-        name: 'pets',
-        slug: 'pets'
-      },
-      {
-        name: 'politics',
-        slug: 'politics'
-      },
-      {
-        name: 'rideshare',
-        slug: 'rideshare'
-      },
-      {
-        name: 'volunteers',
-        slug: 'volunteers'
       }
     ]
   },
@@ -110,8 +86,8 @@ module.exports = [
         slug: 'arch-or-engineering'
       },
       {
-        name: 'art / media / design',
-        slug: 'art-or-media-or-design'
+        name: 'art / media',
+        slug: 'art-or-media'
       },
       {
         name: 'biotech / science',
@@ -132,104 +108,7 @@ module.exports = [
       {
         name: 'food / bev / hosp',
         slug: 'food-or-bev-or-hosp'
-      },
-      {
-        name: 'general labor',
-        slug: 'general-labor'
-      },
-      {
-        name: 'government',
-        slug: 'government'
-      },
-      {
-        name: 'human resources',
-        slug: 'human-resources'
-      },
-      {
-        name: 'internet engineers',
-        slug: 'internet-engineers'
-      },
-      {
-        name: 'legal / paralegal',
-        slug: 'legal-or-paralegal'
-      },
-      {
-        name: 'manufacturing',
-        slug: 'manufacturing'
-      },
-      {
-        name: 'marketing / pr / ad',
-        slug: 'marketing-or-pr-or-ad'
-      },
-      {
-        name: 'medical / health',
-        slug: 'medical-or-health'
-      },
-      {
-        name: 'nonprofit sector',
-        slug: 'nonprofit-sector'
-      },
-      {
-        name: 'real estate',
-        slug: 'real-estate'
-      },
-      {
-        name: 'retail / wholesale',
-        slug: 'retail-or-wholesale'
-      },
-      {
-        name: 'sales / biz dev',
-        slug: 'sales-or-biz-dev'
-      },
-      {
-        name: 'salon / spa / fitness',
-        slug: 'salon-or-spa-or-fitness'
-      },
-      {
-        name: 'security',
-        slug: 'security'
-      },
-      {
-        name: 'skilled trade / craft',
-        slug: 'skilled-trade-or-craft'
-      },
-      {
-        name: 'software / qa / dba',
-        slug: 'software-or-qa-or-dba'
-      },
-      {
-        name: 'systems / network',
-        slug: 'systems-or-network'
-      },
-      {
-        name: 'technical support',
-        slug: 'technical-support'
-      },
-      {
-        name: 'transport',
-        slug: 'transport'
-      },
-      {
-        name: 'transport',
-        slug: 'transport'
-      },
-      {
-        name: 'tv / film / video',
-        slug: 'tv-or-film-or-video'
-      },
-      {
-        name: 'web / info design',
-        slug: 'web-or-info-design'
-      },
-      {
-        name: 'writing / editing',
-        slug: 'writing-or-editing'
-      },
-      {
-        name: 'part-time',
-        slug: 'part-time'
       }
-
     ]
   },
   {
