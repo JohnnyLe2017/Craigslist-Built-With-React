1bc00b4158ab81fd4cb508d522ecd1d5061f4201
Johnny Le <johnnylewebdeveloper@gmail.com>
2018-08-27 09:01:59 -0400

    Commits

--- assets/js/craigslist/craigslist.js
+++ assets/js/craigslist/craigslist.js
@@ -1,9 +1,6 @@
 import React, { Component} from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
-//Import App.js is inside of the same directory called craigslist
-
-
 
 const app = document.getElementById('app')
 
